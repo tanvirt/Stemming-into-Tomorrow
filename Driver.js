bd3f029400420660ddd1cf436fcfb3d8fbef2605
FiveTierAdam <fortieradam@gmail.com>
2016-01-30 14:23:39 -0500

    Merged

--- Driver.js
+++ Driver.js
@@ -1,16 +1,16 @@
 var inputDevice = new LeapMotionInputDevice();
 var canvas = new Canvas("div_container", inputDevice);
-//var hand = new Hand(canvas);
-//hand.addToCanvas();
+var hand = new Hand(canvas);
+hand.addToCanvas();
 
 
 //Testing Cube Class
-
+/*
 var center = new Point(0,0,0);
 
 var cube = new Cube(canvas, center, 0.5);
 cube.addToCanvas();
-
+*/
 
 /*
 // Testing text class
