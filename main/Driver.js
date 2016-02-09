46c0c6c8b28613f0bc811874a2734cdd48dcd65d
tanvirt <tanvirt@ufl.edu>
2016-02-09 02:23:52 -0500

    Reorganized file system. Added webgl-2.6 to lib for modification.

--- main/Driver.js
+++ main/Driver.js
@@ -1,7 +1,7 @@
 var inputDevice = new LeapMotionInputDevice();
 var canvas = new Canvas(inputDevice);
 //canvas.getWebGLCanvas().useRedCyanProjector();
-canvas.setRoom("http://localhost/webapps/Senior_Project/Stemming-into-Tomorrow/default_room");
+canvas.setRoom("http://localhost/webapps/Senior_Project/Stemming-into-Tomorrow/data/rooms/default");
 canvas.getWebGLCanvas().onDrag = function(event) {
 	canvas.getWebGLCanvas().camera.oneFingerRotate(event);
 }
