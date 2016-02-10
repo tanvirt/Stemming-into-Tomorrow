f2489aa33890712503ffc5683ac137ea05a29932
FiveTierAdam <fortieradam@gmail.com>
2016-02-10 00:47:13 -0500

    Added driver.js to gitignore

--- main/Driver.js
+++ main/Driver.js
@@ -1,7 +1,7 @@
 var my_inputDevice = new LeapMotionInputDevice();
 var my_canvas = new Canvas(my_inputDevice);
-//my_canvas.useRedCyanProjector();
-my_canvas.setRoom("http://localhost/webapps/Senior_Project/Stemming-into-Tomorrow/data/rooms/default");
+my_canvas.useRedCyanProjector();
+my_canvas.setRoom("http://localhost:8888/Senior_Project/Stemming-into-Tomorrow/data/rooms/default");
 my_canvas.onDrag = function(event) {
 	my_canvas.getCamera().oneFingerRotate(event);
 }
