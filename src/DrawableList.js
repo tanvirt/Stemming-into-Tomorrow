c1a851ee01ff260e609cf371317734c56666b59a
tanvirt <tanvirt@ufl.edu>
2016-03-15 15:37:44 -0400

    Some updates to DrawableList.

--- src/DrawableList.js
+++ src/DrawableList.js
@@ -6,14 +6,16 @@ DrawableList.prototype.add = function(drawableObject) {
 	this._graphicList[drawableObject.getId()] = drawableObject;
 }
 
-DrawableList.prototype.remove = function(drawableObject) {
-	if(drawableObject.getId() in this._graphicList)
-		delete this._graphicList[drawableObject.getId()];
+DrawableList.prototype.remove = function(objectId) {
+	if(objectId in this._graphicList)
+		delete this._graphicList[objectId];
 }
 
-DrawableList.prototype.getObject = function(id) {
-	if(id in this._graphicList)
-		return this._graphicList[id];
+DrawableList.prototype.getObject = function(objectId) {
+	if(objectId in this._graphicList)
+		return this._graphicList[objectId];
+	else
+		return null;
 }
 
 DrawableList.prototype.draw = function() {
