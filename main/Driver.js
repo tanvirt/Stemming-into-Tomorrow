1169f63a9f3b7c88cc9e41b466ab01358078bca9
FiveTierAdam <fortieradam@gmail.com>
2016-03-24 18:07:34 -0400

    Added Server buffer class to contain Visineat tools.
    	provides reliable public interface(in terms of server var access) 
    	for developers to use

--- main/Driver.js
+++ main/Driver.js
@@ -1,51 +1,69 @@
-var my_inputDevice = new LeapMotionInputDevice();
-var my_canvas = new Canvas(my_inputDevice);
-var host = Host.toString();
-var room = "Senior_Project/Stemming-into-Tomorrow/data/rooms/default";
-my_canvas.setRoom(host + room);
 
-my_canvas.onDrag = function(event) {
-	my_canvas.getCamera().oneFingerRotate(event);
-}
 
-var game = new Game(my_canvas, my_inputDevice);
 
-document.body.onkeypress = function(event) {
-	if(event.keyCode === 49)
-		my_canvas.useRegularProjector();
-	else if(event.keyCode === 50)
-		my_canvas.useRedCyanProjector();
-	else if(event.keyCode === 51)
-		my_canvas.useOculusProjector();
-	else if(event.keyCode === 52)
-		my_canvas.useSideBySideProjector();
-}
-
-window.onorientationchange = function() {
-	if(Mobile.isPhone() && Mobile.isLandscape())
-		my_canvas.useOculusProjector();
-	else
-		my_canvas.useRegularProjector();
-}
-
-var server = new VNServer();
-
-server.onConnectionOpened = function() {
-	server.joinFirstAvailableSession(0, false);
-};
-
-server.onSelfJoinedSession = function(session) {
-	var message = server.currentSession.variable("message");
+function Driver() {
+	var host = Host.toString();
+	var room = "Senior_Project/Stemming-into-Tomorrow/data/rooms/default";
+	var my_inputDevice = new LeapMotionInputDevice();
+	var my_canvas = new Canvas(my_inputDevice);
+	my_canvas.setRoom(host + room);
 	
-	message.onValueChanged = function(message, user) {
-		if(user.Id == server.me().Id)
-			console.log("me: " + message.value());
-		else console.log("other: " + message.value());
-	};
-	
-	document.body.onclick = function(event) {
-		message.set(CanvasMath.generateUniqueString(10));
+	my_canvas.onDrag = function(event) {
+		my_canvas.getCamera().oneFingerRotate(event);
 	}
-};
+	
+	var game = new Game(my_canvas, my_inputDevice);
+	
+	document.body.onkeypress = function(event) {
+		if(event.keyCode === 49)
+			my_canvas.useRegularProjector();
+		else if(event.keyCode === 50)
+			my_canvas.useRedCyanProjector();
+		else if(event.keyCode === 51)
+			my_canvas.useOculusProjector();
+		else if(event.keyCode === 52)
+			my_canvas.useSideBySideProjector();
+	}
+	
+	window.onorientationchange = function() {
+		if(Mobile.isPhone() && Mobile.isLandscape())
+			my_canvas.useOculusProjector();
+		else
+			my_canvas.useRegularProjector();
+	}
+	var variables = ["gameScore", "HandPoints", "Gestures"];
+	var server = new Server(variables, this);
+} 
 
-server.connect("Experiential Learning");
+
+Driver.prototype.acceptnotification = function(server) {
+	server.modifyVariable("gameScore", 5);
+}
+
+var driver = new Driver();
+//var interval = server.setVariable("gameScore");
+//var gameScore = server.getVariable("gameScore");
+//server.initServer();
+//server.modifyGameScore(10);
+
+//var server = new VNServer();
+//
+//server.onConnectionOpened = function() {
+//	server.joinFirstAvailableSession(0, false);
+//};
+//
+//server.onSelfJoinedSession = function(session) {
+//	var message = server.currentSession.variable("message");
+//	
+//	message.onValueChanged = function(message, user) {
+//		if(user.Id == server.me().Id)
+//			console.log("me: " + message.value());
+//		else console.log("other: " + message.value());
+//	};
+//	
+//	document.body.onclick = function(event) {
+//		message.set(CanvasMath.generateUniqueString(10));
+//	}
+//};
+//
+//server.connect("Experiential Learning");
---
+++ src/server/AssociativeArray.js
@@ -0,0 +1,64 @@
+function AssociativeArray() {
+	this._associativeArray = [];
+	this._size = 0;
+}
+
+AssociativeArray.prototype.put = function(key, value) {
+	this._associativeArray[key] = value;
+	this._size++;
+}
+
+AssociativeArray.prototype.putAll = function(associativeArray) {
+	this._associativeArray = associativeArray;
+	for(var key in associativeArray)
+		this._size++;
+}
+
+AssociativeArray.prototype.replace = function(key, value) {
+	if(this.containsKey(key))
+		this._associativeArray[key] = value;
+	else 
+		console.log("Error: key " + key + " does not exist");
+}
+
+AssociativeArray.prototype.containsKey = function(key) {
+	return (this._associativeArray.hasOwnProperty(key));
+}
+
+AssociativeArray.prototype.get = function(key) {
+	if(this.containsKey(key))
+		return this._associativeArray[key];
+	else
+		return null;
+}
+
+AssociativeArray.prototype.getAssociativeArray = function() {
+	return this._associativeArray;
+}
+
+AssociativeArray.prototype.remove = function(key) {
+	if(this.containsKey(key)) {
+		delete this._associativeArray[key];
+		this._size--;
+	}
+}
+
+AssociativeArray.prototype.initWithKeys = function(keyList) {
+	for(var i in keyList) {
+		this.put(keyList[i], "");
+		console.log(keyList[i]);
+	}
+}
+
+AssociativeArray.prototype.size = function() {
+	return size;
+}
+
+
+AssociativeArray.prototype.getKeys = function() {
+	var keys = [];
+	for(var key in this._associativeArray)
+		if(this.containsKey(key))
+			keys.push(key);
+	return keys;
+}
